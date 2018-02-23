7c62b7f1c786dbbda3b93febefe8c818486d0e82
Cyprella <34089622+Cyprella@users.noreply.github.com>
2018-02-23 16:33:29 +1030

    Blocktimes

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -70,8 +70,8 @@ public:
         consensus.BIP34Height = 227931;
         consensus.BIP34Hash = uint256S("0x000000000000024b89b42a942fe0d9fea3bb44ab7bd1b19115dd6a759c0808b8");
         consensus.powLimit = uint256S("00000fffff000000000000000000000000000000000000000000000000000000");
-        consensus.nPowTargetTimespan = 60 * 60; // Suc: 60 minutes 30 blocks
-        consensus.nPowTargetSpacing = 2 * 60; // Suc: 2 minutes
+        consensus.nPowTargetTimespan = 120 * 60; // Suc: 60 minutes 30 blocks
+        consensus.nPowTargetSpacing = 1 * 60; // Suc: 2 minutes
         consensus.fPowAllowMinDifficultyBlocks = false;
         consensus.fPowNoRetargeting = false;
         consensus.nRuleChangeActivationThreshold = 1916; // 95% of 2016
